aa98906887a18f2bc3f4201a21ea7061f6835deb
Thales Nishida <medeiros.tn@gmail.com>
2024-08-02 12:22:08 -0300

    FIm da aula 1 do modulo 4 (Navigation part 2: novas features e reutilizacao de codigo) Criando o ViewModel de estado do App

--- app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import br.com.alura.aluraesporte.R
+import br.com.alura.aluraesporte.ui.viewmodel.EstadoViewModel
 import kotlinx.android.synthetic.main.main_activity.main_activity_nav_host
+import org.koin.android.viewmodel.ext.android.viewModel
 
 
 class MainActivity : AppCompatActivity() {
 
     private val controller: NavController by lazy { findNavController(R.id.main_activity_nav_host)}
+    private val viewModel: EstadoViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

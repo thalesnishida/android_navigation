24b6d60effb74803a4dc5547126fa49fe68ff11c
Thales Nishida <medeiros.tn@gmail.com>
2024-08-02 11:09:57 -0300

    Fim aula 2 modulo 3 (Navigation part 2) Aplicando configurações na mudança de destinos

--- app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/activity/MainActivity.kt
@@ -2,14 +2,29 @@ package br.com.alura.aluraesporte.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import br.com.alura.aluraesporte.R
+import kotlinx.android.synthetic.main.main_activity.main_activity_nav_host
 
 
 class MainActivity : AppCompatActivity() {
 
+    private val controller: NavController by lazy { findNavController(R.id.main_activity_nav_host)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        controller.addOnDestinationChangedListener { controller, destination, arguments ->
+
+            title = destination.label
+
+            when(destination.id) {
+                R.id.listaProdutos -> supportActionBar?.show()
+                R.id.login -> supportActionBar?.hide()
+            }
+
+        }
     }
 
 }
